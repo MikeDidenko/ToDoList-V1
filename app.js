7928e31f901cf7757b99432614dc561cceb03bc4
Michael Didenko <paradox600@gmail.com>
2020-07-10 14:27:44 +0100

    Complete ToDo App

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
+const items = ["Buy Food", "Cook Food"];
+const workItems = [];
+
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-  var today = new Date();
-  var day = "";
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    day = "Weekend";
-  } else {
-    day = "Weekday";
-  }
-  res.render("list", {kindOfDay: day});
+app.get("/", function(req, res) {
+  let day = date.getDate();
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 });
 
+app.post("/", function(req, res) {
+  // console.log(req.body);
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+})
 
+app.get("/about", function(req, res) {
+  res.render("about");
+})
 
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+})
 
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000")
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 })
